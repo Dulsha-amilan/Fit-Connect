001988daf37c3792c415347ff60c08b43f6496fb
Your Name <99183326+IT21168536@users.noreply.github.com>
2024-05-11 21:23:50 +0530

    fg

--- GYM-main/GYM-main/front-end/src/App.js
+++ GYM-main/GYM-main/front-end/src/App.js
@@ -11,7 +11,7 @@ import WorkoutStatusPage from './pages/workoutStatusPage/WorkoutStatusPage';
 import Profile from './pages/profile/Profile';
 import Messages from './pages/messagePage/MessagePage';
 import Chat from './pages/chatPage/ChatPage';
-//cccbbb
+//
 
 function App() {
   return (
